c32b869f1a5fbe30d4454ef3aefae514aa8fd1fe
hobit22 <hobit22@naver.com>
2022-07-10 23:00:11 +0900

    [add] UserCustomRepository 추가

--- src/main/java/com/hh99team11/backend/repository/UserRepository.java
+++ src/main/java/com/hh99team11/backend/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User,Long>, UserCustomRepository {
+public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findById(long l);
--- src/main/java/com/hh99team11/backend/service/MapService.java
+++ src/main/java/com/hh99team11/backend/service/MapService.java
@@ -2,6 +2,7 @@ package com.hh99team11.backend.service;
 
 import com.hh99team11.backend.dto.NearByUserResponseDto;
 import com.hh99team11.backend.model.User;
+import com.hh99team11.backend.repository.UserCustomRepository;
 import com.hh99team11.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,9 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class MapService {
-    private final UserRepository userRepository;
+    private final UserCustomRepository userRepository;
 
     public List<NearByUserResponseDto> getNearByUsers(User user) {
         Long userId = user.getId();
